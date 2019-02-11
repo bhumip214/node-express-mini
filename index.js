41f63e659ec953ddf04e64cc04185a8348b50943
bhumip214 <bhumip214@gmail.com>
2019-02-11 15:47:26 -0800

    Created POST and updated GET by Id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.use(express.json());
 server.get("/api/users", (req, res) => {
   db.find()
     .then(users => {
-      res.status(200).json({ users });
+      res.status(200).json(users);
     })
     .catch(err => {
       res.status(500).json({
@@ -23,12 +23,10 @@ server.get("/api/users", (req, res) => {
 
 //GET by ID request
 server.get("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-
-  db.findById(id)
-    .then(users => {
+  db.findById(req.params.id)
+    .then(user => {
       if (user) {
-        res.status(200).json({ users });
+        res.status(200).json(user);
       } else {
         res.status(404).json({
           message: "The user with the specified ID does not exist."
@@ -42,14 +40,35 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
+// POST request
+
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    db.insert({ name, bio })
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "The users information could not be retrieved."
+        });
+      });
+  }
+});
+
 // DELETE request
 server.delete("/api/users/:id", (req, res) => {
-  const { id } = req.params.id;
+  const id = req.params.id;
 
   db.remove(id)
     .then(user => {
       if (user) {
-        res.status(204).end();
+        res.status(204).json({ message: "The user is successfully deleted" });
       } else {
         res.status(404).json({
           message: "The user with the specified ID does not exist."
